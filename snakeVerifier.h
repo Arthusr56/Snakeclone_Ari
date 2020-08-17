e37e6f458a0248ac62a2a6056d846f9edfde2324
ArthusArion <56237061+Arthusr56@users.noreply.github.com>
2020-08-17 16:23:45 -0500

    Add files via upload

--- snakeVerifier.h
+++ snakeVerifier.h
@@ -3,7 +3,6 @@
 #define snd second
 using namespace std;
 
-
  bool visitedCells[17][19];
 
 void setValids(){
@@ -20,16 +19,34 @@ void setValids(){
 }
 
 
-bool isitValid(int snakeBody[], int currentLength){
+bool isItValid(vector < pair < int , int > > snakeBody(), int currentLength){
 
  /**
   *   Verifies whether or not the position of the snake's body is valid
 
-  *   @param snakeBody[], coordinates occupied by the snake's body
+  *   @param snakeBody(), coordinates occupied by the snake's body
   *   @param currentLength, the current size of the snake
   *   @return true if it's valid, false otherwise
   */
 
  int i, j;
+ int actualRow, actualColumn;
 
+ /*
+ for(i = 0 ; i < currentLength ; i++){
+
+    actualRow = snakeBody[i].fst;
+    actualColumn = snakeBody[i].snd;
+
+    if(visitedCells[actualRow][actualColumn] == true)
+        return false;
+    visitedCells[actualRow][actualColumn] = true;
+ }
+ */
+
+ for(i = 1 ; i < 16 ; i++){
+    for(j = 1 ; j < 18 ; j++)
+       visitedCells[i][j]=false;
+ }
+ return true;
 }
